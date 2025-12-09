5b7e1f5aa1c4e22f90505d19ce9283c0a23cb1a5
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-12-09 16:07:40 +0530

    Repaired useAuth hook

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,9 +1,14 @@
 import api from "@/lib/api";
 import { useEffect, useState } from "react";
 
+type User = {
+  _id: string;
+  username: string;
+};
+
 const useAuth = () => {
   const [loading, setLoading] = useState<boolean>(true);
-  const [user, setUser] = useState<unknown>();
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     api
