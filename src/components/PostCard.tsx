fd90663d25db708b57e286c33fe2702f947109ec
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-12-10 17:33:47 +0530

    Made responsive changes on PostCard.tsx

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 const PostCard = ({ post }: { post: Post }) => {
   return (
-    <div className="group hover:scale-105 rounded flex flex-col justify-center items-start py-12 px-6 w-64 border-8! border-white! transition-all duration-500 hover:shadow-lg! overflow-hidden">
+    <div className="group hover:scale-105 rounded flex flex-col justify-between items-center py-12 px-6 w-64 sm:h-128 border-8! border-white! transition-all duration-500 hover:shadow-lg! overflow-hidden">
       <Image
         src={post.image}
         width={200}
         height={200}
-        className="w-full group-hover:scale-105 transition-transform duration-500"
+        className="w-full sm:h-full group-hover:scale-105 transition-transform duration-500"
         alt="FrameVerse Post"
       />
       <div className="flex flex-col justify-center items-start p-4! group-hover:pt-6! transition-discrete duration-500">
