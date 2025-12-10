fd3850c5d45c87c9dab290fc89f3bc6f68bce24e
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-12-10 23:44:42 +0530

    added toasts for various states

---
+++ src/lib/toasts.ts
@@ -0,0 +1,29 @@
+import { toaster } from "@/components/ui/toaster";
+
+export const successToast = (msg: string) => {
+  toaster.create({
+    title: msg,
+    type: "success",
+  });
+};
+
+export const errorToast = (msg: string) => {
+  toaster.create({
+    title: msg,
+    type: "error",
+  });
+};
+
+export const warnToast = (msg: string) => {
+  toaster.create({
+    title: msg,
+    type: "warning",
+  });
+};
+
+export const infoToast = (msg: string) => {
+  toaster.create({
+    title: msg,
+    type: "info",
+  });
+};
